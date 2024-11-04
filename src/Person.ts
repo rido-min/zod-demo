6c7b8a68da8f4eac9e2488e8a1d73ed16db48090
Rido <rido@no-email.com>
2024-11-03 21:44:00 -0800

    add yarn

--- src/Person.ts
+++ src/Person.ts
@@ -1,8 +1,14 @@
-import { iPersonSchema } from './Person._schema'
+import { iPersonSchema } from './Person._schema.js'
+
+export interface address  {
+  street: string
+  number: number
+}
 
 export interface IPerson {
   name: string
   age?: number | null
+  address?: address
   [x: string] : unknown
 }
 
@@ -10,7 +16,21 @@ export class Person implements IPerson {
   name: string
   age?: number | null
   [x: string]: unknown
+  constructor(name: string ) {
+    this.name = name
+  }
   public static fromJson (json: string): IPerson {
     return iPersonSchema.parse(JSON.parse(json))
   }
+  public static fromLiteral(lit:object) : IPerson {
+    return iPersonSchema.parse(lit)
+  }
+}
+
+export interface IEmployee extends IPerson {
+  salary: number
+}
+
+export class Employee extends Person implements IEmployee {
+  salary: number
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
-import { Person } from './Person'
+import {  IPerson, Person } from './Person.js'
+
+const age = 23
+const streetNumber = 18
+
+const addr  = {
+    street: 'teseo', number: streetNumber
+}
+
+const rido : Person = {
+    name : 'rido',
+    age : 24,
+    addr
+}
+
+const p = new Person('yo')
+
+console.log(p)
+console.log(Person.fromLiteral(rido))
+console.log(JSON.stringify(rido))
 
 const pjson: Person = Person.fromJson('{ "name": "pp", "age" : null, "notdeclared" : "aa" }')
-
 console.log(pjson)
